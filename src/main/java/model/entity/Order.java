49881b81bc6dc33da9e544cb1734e18f87afc112
Kskaixang <wss07715@gmail.com>
2025-04-23 11:44:07 +0800

    1143

--- src/main/java/model/entity/Order.java
+++ src/main/java/model/entity/Order.java
@@ -1,30 +1,16 @@
 package model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
 	private String item;
 	private Integer price;
 	
-	public Order() {		
-	}
-	
-	public Order(String item, Integer price) {
-		super();
-		this.item = item;
-		this.price = price;
-	}
-	public String getItem() {
-		return item;
-	}
-	public void setItem(String item) {
-		this.item = item;
-	}
-	public Integer getPrice() {
-		return price;
-	}
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
 	
 	
 	
